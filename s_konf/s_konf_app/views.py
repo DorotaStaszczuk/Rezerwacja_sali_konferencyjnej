bde3ffffbf3f606587e7bd5806bb67fa466ff64c
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2018-10-21 23:56:53 +0200

    css style added

--- s_konf/s_konf_app/views.py
+++ s_konf/s_konf_app/views.py
@@ -3,8 +3,8 @@ from django.views import View
 
 # Create your views here.
 
+
 class MainSiteView(View):
     def get(self, request):
         ctx = {}
         return render(request, 'conf_template.html', ctx)
-
