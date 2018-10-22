76fd5d67cd776325d04a7f27bfb6dcb660e1e943
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2018-10-22 23:33:37 +0200

    RoomView added

--- s_konf/s_konf_app/views.py
+++ s_konf/s_konf_app/views.py
@@ -1,10 +1,18 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import DetailView
+from s_konf_app.models import Room
 
 # Create your views here.
+
 
 
 class MainSiteView(View):
     def get(self, request):
         ctx = {}
         return render(request, 'conf_template.html', ctx)
+
+
+class RoomView(LoginRequiredMixin, DetailView):
+    model = Room
