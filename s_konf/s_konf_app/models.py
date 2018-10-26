1d527d61b351ad112fdbc3436f5f8421e6183ef5
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2018-10-26 23:43:47 +0200

    verbose_name added

--- s_konf/s_konf_app/models.py
+++ s_konf/s_konf_app/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Room(models.Model):
-    name = models.CharField(max_length=64)
-    capacity = models.IntegerField(null=True)
-    projector = models.BooleanField(default=True)
+    name = models.CharField(max_length=64, verbose_name="nazwa")
+    capacity = models.IntegerField(null=True, verbose_name="pojemność")
+    projector = models.BooleanField(default=True, verbose_name="rzutnik")
 
 
 class Reservation(models.Model):
-    date = models.DateField(unique_for_date=True)
-    comment = models.CharField(max_length=200)
+    date = models.DateField(unique_for_date=True, verbose_name="data")
+    comment = models.CharField(max_length=200, verbose_name="komentarz")
     room_id = models.ForeignKey('Room', on_delete=models.CASCADE)
